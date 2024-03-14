2f37e9de5d35e5206cfc1c0df960920070e8746c
ShreYansHSachan11 <shreyanshsachan2016@gmail.com>
2024-03-15 00:20:21 +0530

    api request updated

--- src/components/apitester.jsx
+++ src/components/apitester.jsx
@@ -3,23 +3,29 @@ import axios from 'axios';
 import photo from '../assets/back1.jpg'
 const Apitester = () => {
   
-  
-  const handleSearch = (e) => {
-        axios.post("https://sherlock-backend-4.onrender.com/filedata",
-          {
-            name:"shreyansh",
-            textdata:"hello good morning to everyone",
-            file:`${photo}`
+       
+    const handleSearch = (e) => {
+        const formData = new FormData();
+        
+        // Append file, name, and textdata to the FormData object
+        formData.append('file', photo);
+        formData.append('name', 'shreyansh');
+        formData.append('textdata', 'hello good morning to everyone');
+      
+        // Make a POST request using FormData
+        axios.post("https://sherlock-backend-4.onrender.com/filedata", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
           }
-        ) .then(function (response) {
+        })
+        .then(function (response) {
           console.log(response);
         })
         .catch(function (error) {
           console.log(error);
         });
-        
+      }
       
-    }
 
   
   
